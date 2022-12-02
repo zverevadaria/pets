81c73bea0bb58df14e4e187eb84914619bbe3529
Daria <47642835+zverevadaria@users.noreply.github.com>
2022-12-02 15:15:39 +0300

    Add files via upload

---
+++ Pets.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Pets {
+
+        String Sex;
+        static String Kind;
+        String petName;
+        int birthDate;
+        String descr;
+        Scanner sr = new Scanner(System.in);
+
+
+        void AddPet() { // idea сама создала
+                System.out.println("Введите тип:");
+                Kind = sr.nextLine();
+                System.out.println("Ваш вид: " + Kind);
+                System.out.println("Ввидите пол ");
+                Sex = sr.nextLine();
+                System.out.println("Ваш пол: " + Sex);
+                System.out.println("Введите имя питомца ");
+                petName = sr.nextLine();
+                System.out.println("Имя питомца: " + petName);
+        }
+
+
+}
+
+
+
